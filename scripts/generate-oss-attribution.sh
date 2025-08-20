4f3ebfa0a2e8e690f0dfecd5c55e83fbfd41c734
VictorPaiu <vpaiu@amazon.com>
2025-08-20 16:31:10 +0200

    Fix bug

--- scripts/generate-oss-attribution.sh
+++ scripts/generate-oss-attribution.sh
@@ -74,7 +74,7 @@ generate_unified_oss_attribution() {
         
         local output
         if [[ -n "$excluded_packages" ]]; then
-            output=$(cd "$ROOT_DIR/code-editor-src-$target" && license-checker --production --exclude MIT,Apache-2.0,BSD-2-Clause,BSD-3-Clause,ISC,0BSD --excludePackages '"$excluded_packages"' 2>/dev/null || true)
+            output=$(cd "$ROOT_DIR/code-editor-src-$target" && license-checker --production --exclude MIT,Apache-2.0,BSD-2-Clause,BSD-3-Clause,ISC,0BSD --excludePackages "'$excluded_packages'" 2>/dev/null || true)
         else
             output=$(cd "$ROOT_DIR/code-editor-src-$target" && license-checker --production --exclude MIT,Apache-2.0,BSD-2-Clause,BSD-3-Clause,ISC,0BSD 2>/dev/null || true)
         fi
